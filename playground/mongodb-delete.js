5cc0e675a21068302e1c5fac0a2db8ab6eb395f7
pcaridade/H <paulocaridade@gmail.com>
2017-11-29 20:50:29 +0000

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,41 @@
+//const MongoClient = require('mongodb').MongoClient;
+//Destructuring assignment
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDb server');
+
+    //deleteMany
+    /*
+    db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result)=>{
+        console.log(result);
+    });
+    */
+
+    //deleteOne
+    /*
+    db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result)=>{
+        console.log(result);
+    });
+    */
+
+    //findOneAndDelete
+    /*
+    db.collection('Todos').findOneAndDelete({completed: false}).then((result)=>{
+        console.log(result);
+    });
+    */
+
+    db.collection('Users').deleteMany({name: 'Paulo'}).then((result)=>{
+        console.log(result);
+    });
+
+    db.collection('Users').findOneAndDelete({_id: new ObjectID("5a1b0740ac0bd00fa8f52b10")}).then((result)=>{
+        console.log(result);
+    });
+
+    //db.close();
+});
