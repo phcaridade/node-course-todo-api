aa4d4979e5654a09f9e01d40bb7688a66bc35914
pcaridade/H <paulocaridade@gmail.com>
2017-12-19 08:10:39 +0000

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,21 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123abc!';
+/*
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+*/
+var hashedPassword = '$2a$10$gxpLdn5k7QykmeqEp28tI.phcjZsCCZ1hYi.KUxYIpDMj4z7IU0be';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+/*
 var data = {
     id: 10
 };
@@ -12,6 +27,7 @@ console.log(token);
 //jwt.verify
 var decoded = jwt.verify(token, '123abc');
 console.log(decoded);
+*/
 
 /*
 var message = 'I am user number 3';
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserShema = new mongoose.Schema(
     {
@@ -80,6 +81,25 @@ UserShema.statics.findByToken = function (token){
 
 
 };
+
+UserShema.pre('save', function(next){
+    var user = this;
+
+    //check if password (any field) was modified
+    if (user.isModified('password')){
+        bcrypt.genSalt(10, (err,salt)=>{
+            bcrypt.hash(user.password, salt, (err, hash)=>{
+                user.password = hash;
+                next();
+            });
+        });
+    }
+    else{
+        next();
+    }
+
+});
+
 
 var User = mongoose.model('User', UserShema);
 
